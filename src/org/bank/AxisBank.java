2a40ebd712f926a13dba9d017ff201803f49976b
puruz <purusothg1313@gmail.com>
2022-03-26 09:32:38 +0530

    bank to git

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,13 @@
+package org.bank;
+
+public class AxisBank extends BankInfo {
+	public void deposit() {
+	System.out.println(12000);
+		
+	}
+	public static void main (String []args) {
+		AxisBank k = new AxisBank();
+		k.deposit();
+	}
+
+}
---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,18 @@
+package org.bank;
+
+public class BankInfo {
+	public void saving() {
+		System.out.println(10000);
+	}
+	public void fixed() {
+		System.out.println(5000);
+	}
+	public void deposit() {
+		System.out.println(7000);
+	}
+	
+
+	
+	
+
+}
